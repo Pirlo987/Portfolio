180e3aa4511ef19ab24bf8ef095aa59497b7e427
Brieuc Vannier <brieuc.vannier931@gmail.com>
2025-03-06 07:51:40 -1000

    correction responsive

--- myapp/components/Home.js
+++ myapp/components/Home.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Header from './Header';
 import Link from 'next/link';
@@ -10,6 +11,11 @@ import Layout from './Layout'; // Import du Layout
 function Home() {
   return (
     <Layout> {/* Ajout du Layout comme wrapper autour de tout le contenu */}
+      <Head>
+        <title>Portfolio Brieuc Vannier</title>
+        <meta name="description" content="Découvrez le portfolio de Brieuc Vannier, développeur frontend et expert en webmarketing & design." />
+      </Head>
+
       <div className={styles.container}>
         <h1 className={styles.title}>Web, Marketing & Design <br /> All in one</h1>
         <p className={styles.paragraphe}>
@@ -19,17 +25,16 @@ function Home() {
         </p>
         
         <div className={styles.network}>
-        <Link href="https://www.linkedin.com/in/brieucvannier/" passHref>
-          <a id={styles.logo} className={styles.activities} target="_blank" rel="noopener noreferrer">
-            <img className={styles.logo} src="Logo/logo-linkedin.png" />
-          </a>
-        </Link>
-        <Link href="https://github.com/Pirlo987" passHref>
-          <a id={styles.logo} className={styles.activities} target="_blank" rel="noopener noreferrer">
-            <img className={styles.logo} src="Logo/github.png" />
-          </a>
-        </Link>
-
+          <Link href="https://www.linkedin.com/in/brieucvannier/" passHref>
+            <a id={styles.logo} className={styles.activities} target="_blank" rel="noopener noreferrer">
+              <img className={styles.logo} src="Logo/logo-linkedin.png" alt="LinkedIn" />
+            </a>
+          </Link>
+          <Link href="https://github.com/Pirlo987" passHref>
+            <a id={styles.logo} className={styles.activities} target="_blank" rel="noopener noreferrer">
+              <img className={styles.logo} src="Logo/github.png" alt="GitHub" />
+            </a>
+          </Link>
         </div>
 
         {/* Flèche qui invite à scroller */}
